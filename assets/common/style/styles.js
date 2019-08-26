7b6d1cafc15a46ed4f1fc810460eede82435af67
g9mein <aver803r803bath261@gmail.com>
2019-08-26 13:04:53 +0800

    feat/global-styles: Add global styles file

--- assets/common/style/styles.js
+++ assets/common/style/styles.js
@@ -4,7 +4,10 @@ export default StyleSheet.create({
   input: {
     borderBottomWidth: 1,
     borderBottomColor: '#555',
-    paddingHorizontal: 10,
+    height: 26,
     fontSize: 20,
+    color: '#000',
+    // paddingHorizontal: 10,
+    // fontSize: 20,
   },
 });
